d1095e2bbccfce47d296d344d819ed36ef841619
Neethi R Nair <neethi1090@gmail.com>
2022-01-14 00:39:45 -0500

    Added youtube trailer view div on click of each movie

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import NavBar from './Components/NavBar/NavBar';
 import Banner from './Components/Banner/Banner';
 import RowPost from './Components/RowPost/RowPost';
-import { originals, action } from './urls';
+import { originals, action, comedy, horror, romance, documentaries } from './urls';
 
 function App() {
   return (
@@ -11,6 +11,10 @@ function App() {
       <Banner />
       <RowPost url={originals} title='Netflix Originals' />
       <RowPost url={action} title='Action' isSmall/>
+      <RowPost url={comedy} title='Comedy' isSmall/>
+      <RowPost url={horror} title='Horror' isSmall/>
+      <RowPost url={romance} title='Romance' isSmall/>
+      <RowPost url={documentaries} title='Documentaries' isSmall/>
     </div>
   );
 }
--- src/Components/RowPost/RowPost.js
+++ src/Components/RowPost/RowPost.js
@@ -1,10 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
+import YouTube from 'react-youtube';
 import './RowPost.css';
 import axios from '../../axios';
 import { API_KEY, imageUrl} from '../../constants/constants';
 
 function RowPost(props) {
     const [movies, setMovies] = useState([]);
+    const [movieUrlId, setMovieUrlId] = useState('');
     useEffect(() => {
         axios.get(`${props.url}`)
         .then((response) => {
@@ -15,17 +17,35 @@ function RowPost(props) {
         })
     }, []);
 
+    const opts = {
+        height: '390',
+        width: '100%',
+        playerVars: {
+          autoplay: 1,
+        },
+    };
+    const handleMovie = (movieId) => {
+        console.log(movieId)
+        axios.get(`movie/${movieId}/videos?api_key=${API_KEY}&language=en-US`)
+        .then((response)=> {
+            if(response.data.results.length!==0) {
+                setMovieUrlId(response.data.results[0].key)
+            }
+        })
+    }
+
     return (
         <div className='rowpost'>
             <h2>{props.title? props.title : ''}</h2>
             <div className="posters">
                 {movies.map((movie) => {
                     return (
-                        <img className={props.isSmall? 'small-poster' : 'poster'} src={`${movie? imageUrl+movie.backdrop_path : ''}`} alt="Poster" />
+                        <img onClick={()=> {handleMovie(movie.id)}} className={props.isSmall? 'small-poster' : 'poster'} src={`${movie? imageUrl+movie.backdrop_path : ''}`} alt="Poster" />
                     )
                 })}
                 
             </div>  
+            { movieUrlId && <YouTube videoId={movieUrlId} opts={opts} />}
         </div>
     )
 }
